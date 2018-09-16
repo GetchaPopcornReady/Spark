2c914dca6078712ed0c2b525d9ae42db26317668
Joshua John Vetter <jvetter@ilstu.edu>
2018-09-16 16:29:09 -0500

    added a create spark view controller

---
+++ Spark/CreateSparkController.swift
@@ -0,0 +1,28 @@
+//
+//  CreateViewController.swift
+//  Spark
+//
+//  Created by Joshua Vetter on 9/15/18.
+//  Copyright © 2018 Joshua Vetter. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import Firebase
+
+class CreateSparkController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("made it to create spark file")
+        
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    
+    
+}
