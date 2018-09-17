2c1e05fc891b8049fbefbee3608765e626d8f409
Joshua John Vetter <jvetter@ilstu.edu>
2018-09-16 22:28:17 -0500

    now have a table controller for sparks

--- Spark/MySparksViewController.swift
+++ Spark/MySparksViewController.swift
@@ -1,28 +1,50 @@
 //
-//  FindSparkViewController.swift
+//  MySparksViewController.swift
 //  Spark
 //
-//  Created by Joshua Vetter on 9/15/18.
+//  Created by Joshua Vetter on 9/16/18.
 //  Copyright © 2018 Joshua Vetter. All rights reserved.
 //
 
-import Foundation
 import UIKit
-import Firebase
 
-class MySparksViewController: UIViewController {
+class MySparksViewController: UIViewController,UITableViewDataSource, UITableViewDelegate {
+
+    let list = ["hi", "hfgh","dghdh","dfhgdgh"]
+    
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return(list.count)
+    }
+    
+     public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "cell")
+        cell.textLabel?.text = list[indexPath.row]
+        
+        return(cell)
+    }
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("made it to My Sparks file")
-        
 
+        print("my sparks loaded")
     }
-    
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
     }
     
-    
-    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
 }
---
+++ Spark/SparksTableViewController.swift
@@ -0,0 +1,88 @@
+//
+//  SparksTableViewController.swift
+//  Spark
+//
+//  Created by Joshua Vetter on 9/16/18.
+//  Copyright © 2018 Joshua Vetter. All rights reserved.
+//
+
+import UIKit
+
+class SparksTableViewController: UITableViewController {
+
+    let list = ["hi", "hfgh","dghdh","dfhgdgh"]
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return(list.count)
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "cell")
+        cell.textLabel?.text = list[indexPath.row]
+        
+        return(cell)
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        
+        print("made it to sparks table view controller")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
